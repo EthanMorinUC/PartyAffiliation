586a91f9fde8df7e6062194458456766cf9943fd
EthanMorinUC <morinej@mail.uc.edu>
2024-10-07 22:55:39 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("What political party are you affiliated with (Democrat, Republican, or Independent ~ First letter of each party name)?");
+        String party = in.nextLine();
+        if (party.equals("D"))
+        {
+            System.out.println("You are Affiliated with Democrat");
+        }
+        else if (party.equals("R"))
+        {
+            System.out.println("You are Affiliated with Republican");
+        }
+        else if (party.equals("I"))
+        {
+            System.out.println("You are Affiliated with Independent");
+        }
+        else
+            System.out.println("You are affiliated with another party");
     }
 }
